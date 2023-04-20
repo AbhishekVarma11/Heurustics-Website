288a70c28a7badc1f8c2ddf4ac26bc7295857823
AbhishekVarma11 <95360581+AbhishekVarma11@users.noreply.github.com>
2023-04-20 13:35:37 +0000

    endpoints with JOSN Data

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Jsonify
 
 app = Flask(__name__)
+JOBS = [{
+  'id': 1,
+  'title': 'Backend Engineer',
+  'location': 'new york',
+  'Salary': '$120,000'
+}, {
+  'id': 2,
+  'title': 'Cloud Engineer',
+  'location': 'California',
+  'Salary': '$150,000'
+}, {
+  'id': 3,
+  'title': 'Devops Engineer',
+  'location': 'Texas',
+  'Salary': '$130,000'
+}, {
+  'id': 4,
+  'title': 'Front-End Engineer',
+  'location': 'Dalas',
+}]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Heuristics')
+
+
+@app.route("/jobs")
+def list_jobs():
+  return Jsonify(JOBS)
 
 
 if __name__ == '__main__':
